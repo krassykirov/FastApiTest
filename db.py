a5dce68bbd75f5899359d5c909f97e559a0cd1c9
Krassy Kirov <krassy.kirov@gmail.com>
2023-08-02 16:45:27 +0300

    ...

--- db.py
+++ db.py
@@ -1,10 +1,10 @@
 from sqlmodel import create_engine, Session
+import os 
+connection = os.getenv("conn_str")
 
-engine = create_engine(
-    "sqlite:///app.db", connect_args={"check_same_thread": False} # Log generated SQL
-    #  "postgresql://fastapi_traefik:fastapi_traefik@db:5432/fastapi_traefik"
-)
+engine = create_engine(connection)
 
+   
 def get_session():
     with Session(engine) as session:
         yield session
